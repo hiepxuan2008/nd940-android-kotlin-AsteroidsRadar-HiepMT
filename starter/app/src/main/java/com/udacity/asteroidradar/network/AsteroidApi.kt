b3bb13ca273f38ad70e536ed537e2dcaab9d259a
hiepxuan2008 <hiepxuan2008@gmail.com>
2021-05-31 18:46:33 +0700

    Move API_KEY to gradle.properties

--- starter/app/src/main/java/com/udacity/asteroidradar/network/AsteroidApi.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/network/AsteroidApi.kt
@@ -1,13 +1,13 @@
 package com.udacity.asteroidradar.network
 
-import com.udacity.asteroidradar.util.Constants
+import com.udacity.asteroidradar.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface AsteroidApi {
     @GET("neo/rest/v1/feed")
     suspend fun getNearEarthObjects(
-        @Query("api_key") apiKey: String = Constants.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): String
 }
 
--- starter/app/src/main/java/com/udacity/asteroidradar/network/PictureOfDayApi.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/network/PictureOfDayApi.kt
@@ -1,13 +1,13 @@
 package com.udacity.asteroidradar.network
 
+import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.models.PictureOfDay
-import com.udacity.asteroidradar.util.Constants
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PictureOfDayApi {
     @GET("planetary/apod")
     suspend fun getAstronomyPictureOfTheDay(
-        @Query("api_key") apiKey: String = Constants.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): PictureOfDay
 }
